38f407e7820d1cdbdf14ec3855576b99788ae5fb
Brian Keane <lonesomewhistle@gmail.com>
2014-06-04 17:32:38 -0500

    initial gemfile, .gitignore, etc setup

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+ruby '2.0.0'
+
+group :development, :test do
+  gem 'rspec'
+  gem 'pry-debugger'
+  gem 'timecop'
+end
+
+gem 'dotenv'
+gem 'ruby-mp3info', '~> 0.8.4'
+gem 'aws-sdk'
---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require './lib/pl.rb'
+require './lib/seed_db.rb'
+require 'rspec'
+require_relative 'shared/shared_database.rb'
+
+
+RSpec.configure do |config|
+  # Configure each test to always use a new singleton instance
+  config.before(:each) do
+    PL::Database.instance_variable_set(:@__db_instance, nil)
+  end
+end
+
+RSpec.configure do |config|
+ # Use color in STDOUT
+   config.color = true
+
+ # Use color not only in STDOUT but also in pagers and files
+   config.tty = true
+
+ # Use the specified formatter
+   config.formatter = :documentation # :progress, :html, :textmate
+end
