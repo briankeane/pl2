51fb908d336503c81d43a80dca99e31339e0761d
Brian Keane <lonesomewhistle@gmail.com>
2014-06-18 17:21:01 -0500

    Added CreateUser and specs

--- spec/use-cases/create_user_spec.rb
+++ spec/use-cases/create_user_spec.rb
@@ -74,6 +74,9 @@ describe 'CreateUser' do
 		expect(result.user.twitter_uid).to eq(5)
 		expect(result.user.email).to eq('bob@bob.com')
 		expect(result.user.birth_year).to eq(1977)
+		expect(result.user.created_at).to be_a(Time)
+		expect(result.user.updated_at).to be_a(Time)
+		binding.pry
 		expect(result.user.gender).to eq('male')
 	end
 
---
+++ spec/use-cases/get_user_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'GetUser' do
+	it 'calls bullshit if the user does not exist' do
+		result = PL::GetUser.run(999)
+		
+		expect(result.success?).to eq(false)
+		expect(result.error).to eq(:user_not_found)
+	end
+
+	it 'gets a user' do
+		user = PL.db.create_user({ twitter: 'bob',
+												twitter_uid: 5,
+												email: 'bob@bob.com',
+												birth_year: 1977,
+												gender: 'male'
+												})
+		result = PL::GetUser.run(user.id)
+
+		expect(result.success?).to eq(true)
+		expect(result.user.id).to eq(user.id)
+		expect(result.user.twitter).to eq('bob')
+		expect(result.user.twitter_uid).to eq(5)
+		expect(result.user.email).to eq('bob@bob.com')
+		expect(result.user.birth_year).to eq(1977)
+		expect(result.user.gender).to eq('male')
+		expect(result.user.created_at).to be_a(Time)
+		expect(result.user.updated_at).to be_a(Time)
+	end
+
+
+
+end
