05802d931d33038832e646ab507645584c5df3c9
Brian Keane <lonesomewhistle@gmail.com>
2014-06-19 18:41:14 -0500

    Added GetSongsByArtist UseCase

---
+++ lib/pl/use-cases/get_songs_by_artist.rb
@@ -0,0 +1,8 @@
+module PL
+	class GetSongsByArtist < UseCase
+		def run(artist)
+			songs_by_artist = PL.db.get_songs_by_artist(artist)
+			return success :songs_by_artist => songs_by_artist
+		end
+	end
+end
