60cfe1402747a07dde38c4cd77eac54f86de3fbf
Brian Keane <lonesomewhistle@gmail.com>
2014-06-19 18:24:15 -0500

    Fixed GetAllSongs spec

--- spec/use-cases/get_all_songs_spec.rb
+++ spec/use-cases/get_all_songs_spec.rb
@@ -11,8 +11,12 @@ describe 'GetAllSongs' do
 		10.times do |i|
 			PL.db.create_song({ title: "#{i} title",
 													artist: "#{i} artist",
-													album: "#{i } album",
+													album: "#{i} album",
 													duration: i })
 		end
+		result = PL::GetAllSongs.run
+		expect(result.success?).to eq(true)
+		expect(result.all_songs.size).to eq(10)
+		expect(result.all_songs[0].title).to eq('0 title')
 	end
 end
