67f8ea19623db79e4b12c41c80bd3f156be8c3c1
Brian Keane <lonesomewhistle@gmail.com>
2014-06-19 19:29:52 -0500

    Added DeleteSong UseCase and tests

---
+++ spec/use-cases/delete_song_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'DeleteSong' do	
+	it 'calls bullshit if the song does not exist' do
+		result = PL::DeleteSong.run(20)
+		expect(result.success?).to eq(false)
+		expect(result.error).to eq(:song_not_found)
+	end
+
+	it 'deletes a song' do
+		song = PL.db.create_song({ artist: 'Brian Keane' })
+		result = PL::DeleteSong.run(song.id)
+		expect(result.success?).to eq(true)
+		expect(result.deleted_song.artist).to eq('Brian Keane')
+	end
+end
