f4e0ea5f3371d4ea8ca69deb176d360285d44cb0
Brian Keane <lonesomewhistle@gmail.com>
2014-06-19 20:18:56 -0500

    Added UpdateSpinFrequency UseCase and specs

---
+++ lib/pl/use-cases/update_spin_frequency.rb
@@ -0,0 +1,18 @@
+module PL
+	class UpdateSpinFrequency < UseCase
+		def run(attrs)
+			song = PL.db.get_song(attrs[:song_id])
+			station = PL.db.get_station(attrs[:station_id])
+
+			case
+			when !song
+				return failure :song_not_found
+			when !station
+				return failure :station_not_found
+			else
+				updated_station = PL.db.update_spin_frequency(attrs)
+				return success :updated_station => updated_station
+			end
+		end
+	end
+end
