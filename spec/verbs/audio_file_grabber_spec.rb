a9afc311fc0aab7d012efe1f016332724fd92f40
briankeane <lonesomewhistle@gmail.com>
2014-06-27 15:06:01 -0500

    Added commentary and commercial functionality to audio_grabber

--- spec/verbs/audio_file_grabber_spec.rb
+++ spec/verbs/audio_file_grabber_spec.rb
@@ -3,8 +3,8 @@ require 'aws-sdk'
 require 'mp3info'
 
 describe 'audio_file_grabber' do
-  it 'grabs a song' do
-    #VCR.use_cassette('audio_file_grabber/grabsong') do
+  it 'grabs song audio' do
+    VCR.use_cassette('audio_file_grabber/grabsong') do
       song = PL.db.create_song({ artist: 'Rachel Loy',
                           album: 'Broken Machine',
                           title: "Good Side",
@@ -23,6 +23,35 @@ describe 'audio_file_grabber' do
       expect(mp3.tag.album).to eq('Broken Machine')
       expect(mp3.tag.artist).to eq('Rachel Loy')
       expect(mp3_file.size).to eq(4895773)
-    #end
+    end
   end
+
+  it 'grabs commentary audio' do
+    VCR.use_cassette('audio_file_grabber/grabcommentary') do
+      commentary = PL.db.create_commentary({ station_id: 1,
+                                            key: 'testCommentary.mp3' })
+
+      grabber = PL::AudioFileGrabber.new
+
+      mp3_file = grabber.grab_audio(commentary)
+
+      expect(mp3_file.size).to eq(497910)
+    end
+  end
+
+  it 'grabs commercial audio' do
+    VCR.use_cassette('audio_file_grabber/grabcommercial') do
+      commercial = PL.db.create_commercial({ sponsor: 'test',
+                                              key: 'testCommercial.mp3' })
+      grabber = PL::AudioFileGrabber.new
+      mp3_file = grabber.grab_audio(commercial)
+
+      expect(mp3_file.size).to eq(128053)
+    end
+
+  end
+
+
+
+
 end
