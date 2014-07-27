7e33c4926a71c38899bf9136b68e15f8c69d8ffb
briankeane <lonesomewhistle@gmail.com>
2014-07-26 20:35:42 -0500

    generate_playlist rewritten but untested

--- lib/pl/entities/schedule.rb
+++ lib/pl/entities/schedule.rb
@@ -1,3 +1,7 @@
+require 'chronic'
+require 'tempfile'
+require 'active_support'
+
 module PL
   class Schedule
   
@@ -9,35 +13,113 @@ module PL
     MS_IN_WEEK = 604.8e+6
     SECONDS_IN_WEEK = MS_IN_WEEK/1000
     SECONDS_IN_DAY = 86400
+    SPINS_WITHOUT_REPEAT = 35
 
-    def generate_playlist
+    def initialize
+      binding.pry
+      Time.zone = @station.timezone
+    end
+
+
+    def generate_playlist(end_time)
       this_thursday_midnight = Chronic.parse('this thursday midnight')
       next_thursday_midnight = this_thursday_midnight + SECONDS_IN_WEEK
       current_playlist = PL.db.get_full_playlist(@id)
+      
+      # keep end_time within range
+      end_time = [end_time, next_thursday_midnight].min unless !end_time
 
       # set max_position and time_tracker initial values
       if current_playlist.size == 0
         max_position = 0
-        time_tracker = Time.now
+        time_tracker = Time.zone.now
       else
         max_position = current_playlist.last.current_position
         time_tracker = self.end_time
       end
 
-      # calibrate commercial_time_block_counter for start-time
-      commercial_time_block_counter = commercial_time_block(time_tracker)
+      # calibrate commercial_counter for start-time
+      commercial_counter = find_commercial_count(time_tracker)
 
-      sample_array = 
+      sample_array = @station.create_sample_array
+
+      recently_played = []
+      spins = []
+
+      while time_tracker < end_time
+
+        # insert time for a commercial if it's time
+        if find_commercial_count(time_tracker) > commercial_counter
+          time_tracker += @station.secs_of_commercial_per_hour
+          commercial_block_counter += 1
+        end
+
+        song = sample_array.song
+
+        # pick again until it hasn't been played recently
+        while recently_played.include?(song)
+          song = sample_array.sample
+        end
+
+        recently_played << song
+
+        # if the recently_played is at max size, delete the first song
+        if ((recently_played.size >= SPINS_WITHOUT_REPEAT) ||
+              (recently_played.size >= @spins_per_week.size - 1))
+          recently_played.shift
+        end
+
+        spin =  Spin.new({ station_id: @station.id,
+                            audio_block_id: song.id,
+                            current_position: (max_position += 1),
+                            estimated_airtime: time_tracker })
+        spins << spin
+        
+        time_tracker += (spin.duration/1000)
+
+      end #end while
+
+      PL.db.mass_insert_spins(spins)
+
+      
+      @original_playlist_end_time = time_tracker
+      @last_accurate_airtime = time_tracker
+      @current_playlist_end_time = time_tracker
     end
+
+    ##################################################################
+    #     insert_commercials                                         #
+    ##################################################################
+    #  Takes in an array of Spin objects.                            #
+    #                                                                #
+    #  Returns an array of Spin Objects with Commercial_Blocks       # 
+    #  inserted and estimated_airtimes corrected                     #
+    #                                                                #
+    #  ASSUMES: 
+    ##################################################################
+    def insert_commercials(spins)
+      # return an empty array if it's empty
+      return [] unless spins
+
+      # set initial commercial_counter
+      commercial_counter = find_commercial_count(spins[0])
+
+      # adjust commercial_block_counter for cases where 1st spin should be a commercial
+      if (self.d)
+      spins.each do |spin|
+
+
+
+
     
 
     def bring_schedule_current
     end
 
     # returns the 'block' number for the given time
-    def commercial_time_block(time)
+    def find_commercial_count(time)
       (time.to_f/1800.0).floor
     end
-  
+    
   end
 end
