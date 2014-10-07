a162602d5289bef813a4ddb32e466e9ed9da96c8
briankeane <lonesomewhistle@gmail.com>
2014-10-07 08:07:01 -0500

    Added to_hash to commentary

--- lib/pl/entities/commentary.rb
+++ lib/pl/entities/commentary.rb
@@ -8,7 +8,7 @@ module PL
 
     def to_hash
       hash = {}
-      self.instance_variables.each { |var| hash[var.to_s.delete('@')] = self.instance_variable_get(var) }
+      self.instance_variables.each { |var| hash[var.to_s.delete('@').to_sym] = self.instance_variable_get(var) }
       hash
     end
   end
--- spec/entities/commentary_spec.rb
+++ spec/entities/commentary_spec.rb
@@ -1,14 +1,25 @@
 require 'spec_helper'
 
 describe 'a commentary' do
+    before(:each) do
+        @commentary = PL::Commentary.new({ id: 1, 
+                                    schedule_id: 2,
+                                    duration: 5000,
+                                    key: 'ThisIsAKey.mp3' })    
+    end
+    
   it 'is created with an id, schedule_id, duration, key' do
-    commentary = PL::Commentary.new({ id: 1, 
-                                schedule_id: 2,
-                                duration: 5000,
-                                key: 'ThisIsAKey.mp3' })
-    expect(commentary.id).to eq(1)
-    expect(commentary.schedule_id).to eq(2)
-    expect(commentary.duration).to eq(5000)
-    expect(commentary.key).to eq('ThisIsAKey.mp3')
+    expect(@commentary.id).to eq(1)
+    expect(@commentary.schedule_id).to eq(2)
+    expect(@commentary.duration).to eq(5000)
+    expect(@commentary.key).to eq('ThisIsAKey.mp3')
+  end
+
+  it 'outputs itself as a hash' do
+    hash = @commentary.to_hash
+    expect(hash[:id]).to eq(1)
+    expect(hash[:schedule_id]).to eq(2)
+    expect(hash[:duration]).to eq(5000)
+    expect(hash[:key]).to eq('ThisIsAKey.mp3')
   end
 end
