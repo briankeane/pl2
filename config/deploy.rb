9c8891c4f56dfc72de32a2f3becf824b669c1585
briankeane <lonesomewhistle@gmail.com>
2014-11-08 16:28:16 -0600

    capistrano tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,7 +38,7 @@ set :branch, ENV['BRANCH'] || "master"
 
 set :deploy_to, '/home/deploy/pl2'
 
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
