a406c87721153ff89b315ccc571ebaa44ae00460
briankeane <lonesomewhistle@gmail.com>
2014-11-20 13:33:29 -0600

    DeleteUnprocessedSong UseCase works with tests

--- lib/pl2/lib/pl/use-cases/delete_unprocessed_song.rb
+++ lib/pl2/lib/pl/use-cases/delete_unprocessed_song.rb
@@ -2,8 +2,12 @@ module PL
   class DeleteUnprocessedSong < UseCase
     def run(key)
       handler = PL::AudioFileStorageHandler.new
-      handler.delete_unprocessed_song(key)
-      return success
+      if handler.unprocessed_song_exists?(key)
+        handler.delete_unprocessed_song(key)
+        return success
+      else
+        return failure :unprocessed_song_not_found
+      end
     end
   end
 end
--- lib/pl2/spec/use-cases/delete_unprocessed_song_spec.rb
+++ lib/pl2/spec/use-cases/delete_unprocessed_song_spec.rb
@@ -1,14 +1,30 @@
 require 'spec_helper'
-
 require 'aws-sdk'
 
 describe 'DeleteUnprocessedSong' do
+  before(:each) do
+    @s3 = AWS::S3.new
+    @grabber = PL::AudioFileStorageHandler.new
+    @s3.buckets['playolaunprocessedsongstest'].objects.delete_all
+  end    
+
+  it 'calls bullshit if the unprocessed song does not exist' do
+    result = PL::DeleteUnprocessedSong.run('asdfsadfasdffasd')
+    expect(result.success?).to eq(false)
+    expect(result.error).to eq(:unprocessed_song_not_found)
+  end
+
   it 'deletes unprocessed song' do
     s3 = AWS::S3.new
-    song = s3.buckets[S3['SONGS_BUCKET']].objects.create('key', 'data')
+    song = s3.buckets['playolaunprocessedsongstest'].objects.create('key', 'data')
     song.write('hi')
     result = PL::DeleteUnprocessedSong.run('key')
     expect(result.success?).to eq(true)
     expect(s3.buckets['playolaunprocessedsongstest'].objects['key'].exists?).to eq(false)
   end
+  
+  after(:each) do
+    @s3.buckets['playolaunprocessedsongstest'].objects.delete_all
+  end
+
 end
