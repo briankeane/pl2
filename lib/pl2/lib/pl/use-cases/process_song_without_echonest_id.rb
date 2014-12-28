1f76870c9d34f0f585738471e32a9c23e176006a
briankeane <lonesomewhistle@gmail.com>
2014-12-28 09:54:34 -0600

    process_song_without_echonest_id use-case bugs

--- lib/pl2/lib/pl/use-cases/process_song_without_echonest_id.rb
+++ lib/pl2/lib/pl/use-cases/process_song_without_echonest_id.rb
@@ -18,33 +18,31 @@ module PL
       when (extension == '.mp4') || (extension == '.m4a') || (extension == '.m4p')
         tags = sp.get_id4_tags(temp_song_file)
       end
-      
-      id3_tags = sp.get_id3_tags(temp_song_file)
 
       tags_to_fix = {}
 
-      # correct id3_tags if necessary
-      if (attrs[:artist] && (attrs[:artist] != id3_tags.artist))
+      # correct tags if necessary
+      if (attrs[:artist] && (attrs[:artist] != tags.artist))
         tags_to_fix[:artist] = attrs[:artist]
-        id3_tags[:artist] = attrs[:artist]
+        tags[:artist] = attrs[:artist]
       end
 
-      if (attrs[:title] && (attrs[:title] != id3_tags.title))
+      if (attrs[:title] && (attrs[:title] != tags.title))
         tags_to_fix[:title] = attrs[:title]
-        id3_tags[:title] = attrs[:title]
+        tags[:title] = attrs[:title]
       end
 
-      if (attrs[:album] && (attrs[:album] != id3_tags.album))
+      if (attrs[:album] && (attrs[:album] != tags.album))
         tags_to_fix[:album] = attrs[:album]
-        id3_tags[:album] = attrs[:album]
+        tags[:album] = attrs[:album]
       end
 
       if tags_to_fix.size > 0
         tags_to_fix[:song_file] = temp_song_file
-        sp.write_id3_tags(tags_to_fix)
+        sp.write_tags(tags_to_fix)
       end
 
-      if PL.db.song_exists?(id3_tags)
+      if PL.db.song_exists?(tags)
         return failure :song_already_exists
       end
 
